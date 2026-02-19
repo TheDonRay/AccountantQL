c2ba7c8b123b4ac125c38a1623fee9acf49274aa
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-19 18:55:04 -0500

    setting up the actual server

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 const { app, express } = require('./app.js'); 
 const apolloserver = require('./graphql/Apolloserver.js'); 
 // express middleware to handle 
-const { expressMiddleWare } = require('@apollo/server/expressv5'); 
+const { expressMiddleware } = require('@apollo/server/expressv4'); 
 
