4923fe1de0a350977a4f2fc438d058c5d20b3610
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-02-24 00:03:43 -0500

    working on the backend here for this project

---
+++ backend/graphql/Resolvers/finances.resolvers.js
@@ -0,0 +1,11 @@
+// this is for my resolvers here 
+// remember we only have resolvers for whereever we have a mutation query or input etc.  
+
+const FinanceResolvers = { 
+    Query: { 
+        // first resolver is going to be the rays finances which gets data from the database 
+        raysFinances: async () => { 
+            
+        }
+    }
+}
--- backend/model/financedata.model.js
+++ backend/model/financedata.model.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose'); 
 
-const FinanceSchema = new mongoose.Schema({ 
-
+// need to have another schema to hold credit card bill data as well 
+const creditcardSchema = new mongoose.Schema({ 
+    bill: { 
+        required: true, 
+        type: Number, 
+    }, 
+    dueDate: { 
+        required: true, 
+        type: String, 
+    }, 
 }); 
 
+const FinanceModel = new mongoose.Schema({ 
+    // we will use the credit card schema as it will follow the Finances code structure here 
+    accountTotal: { 
+        required: true, 
+        type: Number, 
+    }, 
+    creditCards: [creditcardSchema], 
+    goalAmt: { 
+        required: true, 
+        type: Number, 
+    }, 
+    accountantAnalysis: { 
+        required: true, 
+        type: String, 
+    }, 
+})
 
-module.exports = FinanceSchema; 
+
+module.exports = mongoose.model('accountantmodel', FinanceModel); 
